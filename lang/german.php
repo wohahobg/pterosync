e14707c600e42d59d6a9446755e4f6acecff4e45
Aleksandar Ovcharov <wohahobg@gmail.com>
2023-12-30 17:35:31 +0000

    Added german language.

---
+++ lang/german.php
@@ -0,0 +1,10 @@
+<?php
+
+$keys = [
+    'GOTO_PANEL' => "ZUM KONTROLLPANEL GEHEN",
+    'QUICK_ACTIONS_PANEL' => "SCHNELLAUSWAHL",
+    'SERVER_START_PANEL' => "SERVER STARTEN",
+    'SERVER_RESTART_PANEL' => "SERVER NEUSTARTEN",
+    'SERVER_STOP_PANEL' => "SERVER STOPPEN",
+    'SERVER_NOT_FOUND' => "Dieser Server existiert nicht. Bitte kontaktieren Sie uns, wenn Sie glauben, dass dies ein Fehler ist."
+];
