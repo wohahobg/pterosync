3e27294ae00e96a202d8d0cf1cdd3d7095dddc52
Jesus Garcia A <40673956+garciaqjesus@users.noreply.github.com>
2024-07-02 17:50:59 +0200

    Added Spanish

---
+++ pterosync/lang/spanish.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * WHMCS Language File
+ * Spanish (es)
+ *
+ * Please Note: These language files are overwritten during software updates
+ * and therefore editing of these files directly is not advised. Instead we
+ * recommend that you use overrides to customise the text displayed in a way
+ * which will be safely preserved through the upgrade process.
+ *
+ * For instructions on overrides, please visit:
+ *   https://developers.whmcs.com/languages/overrides/
+ *
+ * @package    WHMCS
+ * @author     WHMCS Limited <development@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Limited 2005-2018
+ * @license    https://www.whmcs.com/license/ WHMCS Eula
+ * @version    $Id$
+ * @link       https://www.whmcs.com/
+ */
+
+$keys = [
+    'GOTO_PANEL' => "IR AL PANEL DE CONTROL", 
+    'QUICK_ACTIONS_PANEL' => "ACCIONES RAPIDAS",
+    'SERVER_START_PANEL' => "INICIAR SERVIDOR",
+    'SERVER_RESTART_PANEL' => "REINICIAR SERVIDOR",
+    'SERVER_STOP_PANEL' => "DETENER SERVIDOR",
+    'SERVER_KILL_PANEL' => "FORZAR DETENER SERVIDOR",
+    "SERVER_START_PANEL_CONFIRM_MESSAGE" => "Estas seguro de que deseas iniciar este servidor?",
+    "SERVER_RESTART_PANEL_CONFIRM_MESSAGE" => "Estas seguro de que deseas reiniciar este servidor?",
+    "SERVER_STOP_PANEL_CONFIRM_MESSAGE" => "Estas seguro de que deseas detener este servidor?",
+    "SERVER_KILL_PANEL_CONFIRM_MESSAGE" => "Forzar la detencion del servidor puede llevar a la corrupcion de datos.",
+    'SERVER_NOT_FOUND' => "Este servidor no existe. Por favor, contactanos si crees que esto es un error",
+    "IP_COPIED" => "IP del servidor copiada!",
+    "ID_COPIED" => "ID del servidor copiada!",
+    "SERVER_FTP_INFORMATION" => "Informacion FTP",
+    "SERVER_FTP_PASSWORD_INFORMATION" => "Tenga en cuenta que la clave de FTP es la misma que utiliza para acceder al panel de control.",
+    "SERVER_FTP_USERNAME_COPIED" => "Nombre de usuario copiado!",
+    "SERVER_FTP_HOST_COPIED" => "Host FTP copiado!",
+    "SERVER_FTP_HOST_AND_PORT" => "Host FTP + Puerto",
+    "SERVER_FTP_USERNAME" => "Nombre de usuario",
+    "SERVER_IP" => "IP del servidor",
+    "SERVER_ID" => "ID del servidor",
+    "FTP_DETAILS" => "Detalles FTP"
+];
