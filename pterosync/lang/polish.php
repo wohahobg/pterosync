959ac1dd1e073baa0021ace3d3b7fa46146d8656
MasterCatPL <107404623+MasterCatPL@users.noreply.github.com>
2024-07-13 20:43:30 +0200

    Added polish

---
+++ pterosync/lang/polish.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * WHMCS Language File
+ * Polish (pl)
+ *
+ * Please Note: These language files are overwritten during software updates
+ * and therefore editing of these files directly is not advised. Instead we
+ * recommend that you use overrides to customise the text displayed in a way
+ * which will be safely preserved through the upgrade process.
+ *
+ * For instructions on overrides, please visit:
+ *   https://developers.whmcs.com/languages/overrides/
+ *
+ * @package    WHMCS
+ * @author     WHMCS Limited <development@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Limited 2005-2018
+ * @license    https://www.whmcs.com/license/ WHMCS Eula
+ * @version    $Id$
+ * @link       https://www.whmcs.com/
+ */
+
+$keys = [
+    'GOTO_PANEL' => "PRZEJDŹ DO PANELU STEROWANIA",
+    'QUICK_ACTIONS_PANEL' => "SZYBKIE DZIAŁANIA",
+    'SERVER_START_PANEL' => "URUCHOM SERWER",
+    'SERVER_RESTART_PANEL' => "ZRESERTUJ SERWER",
+    'SERVER_STOP_PANEL' => "ZATRZYMAJ SERWER",
+    'SERVER_KILL_PANEL' => "ZABIJ SERWER",
+    "SERVER_START_PANEL_CONFIRM_MESSAGE" => "Czy napewno chcesz uruchomić ten serwer?",
+    "SERVER_RESTART_PANEL_CONFIRM_MESSAGE" => "Czy napewno chcesz zresertować ten serwer?",
+    "SERVER_STOP_PANEL_CONFIRM_MESSAGE" => "Czy na pewno chcesz zatrzymać ten serwer?",
+    "SERVER_KILL_PANEL_CONFIRM_MESSAGE" => "Wymuszone zatrzymanie serwera może spowodować uszkodzenie danych.",
+    'SERVER_NOT_FOUND' => "Ten serwer nie istneje. Jeśli uważasz, że to pomyłka, skontaktuj się z nami.",
+    "IP_COPIED" => "Serwer IP Skopiowany!",
+    "ID_COPIED" => "Serwer ID Skopiowany!",
+    "SERVER_FTP_INFORMATION" => "Informacje o FTP",
+    "SERVER_FTP_PASSWORD_INFORMATION" => "Proszę zapamiętać, że hasło FTP jest takie same, którego używasz do uzyskania dostępu do panelu sterowania!",
+    "SERVER_FTP_USERNAME_COPIED" => "Użytkownik skopiowany!",
+    "SERVER_FTP_HOST_COPIED" => "FTP Host skopiowany!",
+    "SERVER_FTP_HOST_AND_PORT" => "FTP Host + Port",
+    "SERVER_FTP_USERNAME" => "Użytkownik",
+    "SERVER_IP" => "Serwer IP",
+    "SERVER_ID" => "Serwer ID",
+    "FTP_DETAILS" => "Szczegóły FTP"
+];
