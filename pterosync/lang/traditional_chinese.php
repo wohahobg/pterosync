9693f90a785ec51724c21d9e8b8f12998b3fdd59
FanYueee <84763600+FanYueee@users.noreply.github.com>
2024-07-26 00:11:20 +0800

    Create traditional_chinese.php

---
+++ pterosync/lang/traditional_chinese.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * WHMCS Language File
+ * English (en)
+ *
+ * Please Note: These language files are overwritten during software updates
+ * and therefore editing of these files directly is not advised. Instead we
+ * recommend that you use overrides to customise the text displayed in a way
+ * which will be safely preserved through the upgrade process.
+ *
+ * For instructions on overrides, please visit:
+ *   https://developers.whmcs.com/languages/overrides/
+ *
+ * @package    WHMCS
+ * @author     WHMCS Limited <development@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Limited 2005-2018
+ * @license    https://www.whmcs.com/license/ WHMCS Eula
+ * @version    $Id$
+ * @link       https://www.whmcs.com/
+ */
+
+$keys = [
+    'GOTO_PANEL' => "前往控制面板",
+    'QUICK_ACTIONS_PANEL' => "快速控制",
+    'SERVER_START_PANEL' => "啟動伺服器",
+    'SERVER_RESTART_PANEL' => "重新啟動伺服器",
+    'SERVER_STOP_PANEL' => "停止伺服器",
+    'SERVER_KILL_PANEL' => "強制停止伺服器",
+    "SERVER_START_PANEL_CONFIRM_MESSAGE" => "您確定要啟動這台伺服器嗎？",
+    "SERVER_RESTART_PANEL_CONFIRM_MESSAGE" => "您確定要重新啟動這台伺服器嗎？",
+    "SERVER_STOP_PANEL_CONFIRM_MESSAGE" => "您確定要停止這台伺服器嗎？",
+    "SERVER_KILL_PANEL_CONFIRM_MESSAGE" => "強制停止伺服器可能導致資料損壞。",
+    'SERVER_NOT_FOUND' => "此伺服器不存在。如果您認為這是一個錯誤，請聯繫我們。",
+    "IP_COPIED" => "伺服器 IP 已複製！",
+    "ID_COPIED" => "伺服器 ID 已複製！",
+    "SERVER_FTP_INFORMATION" => "FTP 資訊",
+    "SERVER_FTP_PASSWORD_INFORMATION" => "請注意，FTP 密碼與您的控制面板密碼相同！",
+    "SERVER_FTP_USERNAME_COPIED" => "用戶名已複製！",
+    "SERVER_FTP_HOST_COPIED" => "FTP 連線位置已複製！",
+    "SERVER_FTP_HOST_AND_PORT" => "FTP 連線位置和端口",
+    "SERVER_FTP_USERNAME" => "用戶名",
+    "SERVER_IP" => "伺服器 IP",
+    "SERVER_ID" => "伺服器 ID",
+    "FTP_DETAILS" => "FTP 詳細資訊"
+];
